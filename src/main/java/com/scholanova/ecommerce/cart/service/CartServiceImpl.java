9eb8e308618396e023cdd60227b5700742be0c9c
Florian <florianbematol@gmail.com>
2020-04-15 11:43:13 +0200

    Ecriture du code de CartServiceImple

--- src/main/java/com/scholanova/ecommerce/cart/service/CartServiceImpl.java
+++ src/main/java/com/scholanova/ecommerce/cart/service/CartServiceImpl.java
@@ -1,20 +1,36 @@
 package com.scholanova.ecommerce.cart.service;
 
 import com.scholanova.ecommerce.cart.entity.Cart;
+import com.scholanova.ecommerce.cart.exception.CartException;
+import com.scholanova.ecommerce.cart.repository.CartRepository;
+import com.scholanova.ecommerce.product.entity.Product;
+import com.scholanova.ecommerce.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CartServiceImpl implements CartService{
 
+    @Autowired
+    ProductRepository productRepository;
+
     @Override
-    public Cart addProductToCart(Cart cart, Long productId, int quantity) {
-        //TODO
-        return null;
+    public Cart addProductToCart(Cart cart, Long productId, int quantity) throws CartException {
+        try {
+            Product product = productRepository.findById(productId).get();
+            return cart.addProduct(product, quantity);
+        } catch ( Exception e ) {
+            throw new CartException("Unable to add a product to the cart");
+        }
     }
 
     @Override
-    public Cart changeProductQuantity(Cart cart, Long productId, int quantity) {
-        //TODO
-        return null;
+    public Cart changeProductQuantity(Cart cart, Long productId, int quantity) throws CartException {
+        try {
+            Product product = productRepository.findById(productId).get();
+            return cart.changeProductQuantity(product, quantity);
+        } catch ( Exception e ) {
+            throw new CartException("Unable to change product quantity");
+        }
     }
 }
