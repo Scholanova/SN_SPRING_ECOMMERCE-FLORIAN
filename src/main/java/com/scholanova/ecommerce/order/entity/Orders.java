675ec832bfa8fcf243fc778943393eef7a21b89a
Florian <florianbematol@gmail.com>
2020-04-15 16:01:46 +0200

    Ecriture test Order entity

--- src/main/java/com/scholanova/ecommerce/order/entity/Orders.java
+++ src/main/java/com/scholanova/ecommerce/order/entity/Orders.java
@@ -2,10 +2,13 @@ package com.scholanova.ecommerce.order.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.scholanova.ecommerce.cart.entity.Cart;
-import com.sun.xml.bind.v2.TODO;
+import com.scholanova.ecommerce.order.exception.NotAllowedException;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.sql.Date;
+import java.time.Clock;
+import java.time.LocalDateTime;
 
 @Entity(name="orders")
 public class Orders {
@@ -28,27 +31,37 @@ public class Orders {
     @JoinColumn(name="cart_id", referencedColumnName = "id")
     private Cart cart;
 
-    public Orders() {
-    }
+    public Orders() {}
 
     public void createOrder(){
-        //TODO
+        this.cart = new Cart();
+        this.status = OrderStatus.CREATED;
     }
 
-    public void checkout(){
-        //TODO
+    public void checkout(Clock clock) throws NotAllowedException {
+        if ( this.status.equals(OrderStatus.CLOSED) ) {
+            throw new NotAllowedException("Cannot checkout closed order");
+        }
+
+        if ( this.cart != null && this.cart.getCartItems().stream().map( item -> item.getQuantity()).mapToInt(Integer::intValue).sum() == 0) {
+            throw new IllegalArgumentException("Cannot checkout order when cart got 0 items.");
+        }
+
+        this.issueDate = Date.valueOf(LocalDateTime.now(clock).toLocalDate());
+        this.status = OrderStatus.PENDING;
     }
 
-    public void getDiscount(){
-        //TODO
+    public BigDecimal getDiscount(){
+        BigDecimal total = this.cart.getTotalPrice();
+        return total.compareTo(new BigDecimal(100)) == -1 ? new BigDecimal(0) : (total.multiply(new BigDecimal(5))).divideToIntegralValue(new BigDecimal(100));
     }
 
-    public void getOrderPrice(){
-        //TODO
+    public BigDecimal getOrderPrice(){
+        return this.cart.getTotalPrice().subtract(this.getDiscount());
     }
 
     public void close(){
-        //TODO
+        this.status = OrderStatus.CLOSED;
     }
 
 
@@ -78,7 +91,10 @@ public class Orders {
 
     public Cart getCart() {return cart;}
 
-    public void setCart(Cart cart) {
+    public void setCart(Cart cart) throws NotAllowedException {
+        if ( this.status.equals(OrderStatus.CLOSED) ) {
+            throw new NotAllowedException("Cannot change cart for a closed order.");
+        }
         this.cart = cart;
     }
 }
--- src/test/java/com/scholanova/ecommerce/order/entity/OrdersTest.java
+++ src/test/java/com/scholanova/ecommerce/order/entity/OrdersTest.java
@@ -1,66 +1,142 @@
 package com.scholanova.ecommerce.order.entity;
 
-import org.junit.jupiter.api.Disabled;
+import com.scholanova.ecommerce.cart.entity.Cart;
+import com.scholanova.ecommerce.order.exception.NotAllowedException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
-@Disabled
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.when;
+import static org.mockito.Mockito.mock;
+
+
 class OrdersTest {
 
     @Test
-    public void checkout_ShouldSetTheDateAndTimeOfTodayInTheOrder(){
-
+    public void checkout_ShouldSetTheDateAndTimeOfTodayInTheOrder() throws Exception {
+        //given
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+        Date date = Date.valueOf(LocalDateTime.now(clock).toLocalDate());
+        Orders order = new Orders();
+        //when
+        order.checkout(clock);
+        //then
+        assertThat(order.getIssueDate()).isEqualTo(date);
     }
 
     @Test
-    public void checkout_ShouldSetOrderStatusToPending(){
-
+    public void checkout_ShouldSetOrderStatusToPending() throws NotAllowedException {
+        //given
+        Orders order = new Orders();
+        //when
+        order.checkout(Clock.systemDefaultZone());
+        //then
+        assertThat(order.getStatus()).isEqualTo(OrderStatus.PENDING);
     }
 
     @Test
     public void checkout_ShouldThrowNotAllowedExceptionIfStatusIsClosed(){
-
+        //given
+        Orders order = new Orders();
+        order.setStatus(OrderStatus.CLOSED);
+        //then
+        assertThrows(NotAllowedException.class, () -> order.checkout(Clock.systemDefaultZone()));
     }
 
     @Test
-    public void checkout_ShouldThrowIllegalArgExceptionIfCartTotalItemsQuantityIsZERO(){
-
+    public void checkout_ShouldThrowIllegalArgExceptionIfCartTotalItemsQuantityIsZERO() throws NotAllowedException {
+        //given
+        Orders order = new Orders();
+        Cart cart = new Cart();
+        order.setCart(cart);
+        //then
+        assertThrows(IllegalArgumentException.class, () -> order.checkout(Clock.systemDefaultZone()));
     }
 
     @Test
     public void setCart_ShouldThrowNotAllowedExceptionIfStatusIsClosed(){
-
+        //given
+        Orders order = new Orders();
+        order.setStatus(OrderStatus.CLOSED);
+        Cart cart = new Cart();
+        //then
+        assertThrows(NotAllowedException.class, () -> order.setCart(cart));
     }
 
     @Test
     public void createOrder_ShouldSetTheCartInTheOrder(){
-
+        //given
+        Orders order = new Orders();
+        //when
+        order.createOrder();
+        //then
+        assertThat(order.getCart()).isNotNull();
     }
 
     @Test
     public void createOrder_ShouldSetStatusToCreated(){
-
+        //given
+        Orders order = new Orders();
+        //when
+        order.createOrder();
+        //then
+        assertThat(order.getStatus()).isEqualTo(OrderStatus.CREATED);
     }
 
     @Test
-    public void getDiscount_shouldReturnZEROIFCartTotalPriceIsLessThan100(){
-
+    public void getDiscount_shouldReturnZEROIFCartTotalPriceIsLessThan100() throws NotAllowedException {
+        //given
+        Orders orders = new Orders();
+        Cart cart = mock(Cart.class);
+        orders.setCart(cart);
+        when(cart.getTotalPrice()).thenReturn(new BigDecimal(99));
+        //when
+        BigDecimal res = orders.getDiscount();
+        //then
+        assertThat(res).isEqualTo(new BigDecimal(0));
     }
 
     @Test
-    public void getDiscount_shouldReturn5percentIfCartTotalPriceIsMoreOrEqual100(){
-
+    public void getDiscount_shouldReturn5percentIfCartTotalPriceIsMoreOrEqual100() throws NotAllowedException {
+        //given
+        Orders orders = new Orders();
+        Cart cart = mock(Cart.class);
+        orders.setCart(cart);
+        when(cart.getTotalPrice()).thenReturn(new BigDecimal(100));
+        //when
+        BigDecimal res = orders.getDiscount();
+        //then
+        assertThat(res).isEqualTo(new BigDecimal(5));
     }
 
     @Test
-    public void getOrderPrice_shouldReturnTotalPriceWithDiscount(){
-
+    public void getOrderPrice_shouldReturnTotalPriceWithDiscount() throws NotAllowedException {
+        //given
+        Orders orders = new Orders();
+        Cart cart = mock(Cart.class);
+        orders.setCart(cart);
+        when(cart.getTotalPrice()).thenReturn(new BigDecimal(100));
+        //when
+        BigDecimal res = orders.getOrderPrice();
+        //then
+        assertThat(res).isEqualTo(new BigDecimal(95));
     }
 
     @Test
     public void close_ShouldSetStatusToClose(){
-
+        //given
+        Orders orders = new Orders();
+        //when
+        orders.close();
+        //then
+        assertThat(orders.getStatus()).isEqualTo(OrderStatus.CLOSED);
     }
 
 }
